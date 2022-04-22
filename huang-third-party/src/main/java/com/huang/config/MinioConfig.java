c9389a86b0c7158239beb6789f272186b6c6ac08
Huang <mail@huanghong.top>
2022-04-22 12:01:08 +0800

    concordance minio

---
+++ huang-third-party/src/main/java/com/huang/config/MinioConfig.java
@@ -0,0 +1,34 @@
+package com.huang.config;
+
+import com.huang.properties.MinioProperties;
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Time 2022-04-22 11:52
+ * Created by Huang
+ * className: MinioConfig
+ * Description:
+ */
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class)
+public class MinioConfig {
+
+    @Autowired
+    private MinioProperties minioProperties;
+
+    /**
+     * 获取MinioClient
+     */
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+
+}
