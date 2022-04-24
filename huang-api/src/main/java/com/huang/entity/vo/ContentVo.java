5e4e0ef298ce9e3a3918cde61988da7de0e8bf6b
Huang <mail@huanghong.top>
2022-04-24 21:16:45 +0800

    bug fix

--- huang-api/src/main/java/com/huang/entity/vo/ContentVo.java
+++ huang-api/src/main/java/com/huang/entity/vo/ContentVo.java
@@ -23,7 +23,7 @@ public class ContentVo {
     private Integer topPriority;
     private PostStatus status;
     private String createBy;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT + 8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
     private String updateBy;
     private Date updateTime;
--- huang-api/src/main/java/com/huang/entity/vo/PostVo.java
+++ huang-api/src/main/java/com/huang/entity/vo/PostVo.java
@@ -27,7 +27,7 @@ public class PostVo {
     private Integer visit;
     private Integer deleted;
     private String createBy;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT + 8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
     private String updateBy;
     private Date updateTime;
