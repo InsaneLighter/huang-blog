b036ae12f88e20c620d8b9384efddc92816a1aa9
Huang <mail@huanghong.top>
2022-05-11 20:18:59 +0800

    sort out files

--- huang-common/src/main/java/com/huang/exception/AuthenticationException.java
+++ huang-common/src/main/java/com/huang/exception/AuthenticationException.java
@@ -1,14 +1,12 @@
 package com.huang.exception;
 
-import org.springframework.http.HttpStatus;
-
 /**
- * Authentication exception.
- *
- * @author johnniang
+ * @Time 2022-05-11 20:17
+ * Created by Huang
+ * className: AuthenticationException
+ * Description:
  */
-public class AuthenticationException extends RuntimeException {
-
+public class AuthenticationException extends RuntimeException{
     public AuthenticationException(String message) {
         super(message);
     }
--- huang-third-party/src/main/java/com/huang/config/MinioConfig.java
+++ huang-third-party/src/main/java/com/huang/config/MinioConfig.java
@@ -16,7 +16,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(MinioProperties.class)
 public class MinioConfig {
-
     @Autowired
     private MinioProperties minioProperties;
 
