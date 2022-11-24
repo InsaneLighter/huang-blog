f51c639b4586c85c685d1a87cd5d4688db9ce1bb
Huang <mail@huanghong.top>
2022-11-24 20:11:32 +0800

    common update

--- huang-api/src/main/java/com/huang/entity/vo/ContentVo.java
+++ huang-api/src/main/java/com/huang/entity/vo/ContentVo.java
@@ -26,6 +26,7 @@ public class ContentVo {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
     private String updateBy;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
     private List<String> categoryIds;
     private List<String> tagIds;
